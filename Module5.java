19e059b5960ee3022d6c01e12f2abf95fb6ab08c
Kaklyi <karelyttrevino@gmail.com>
2026-06-14 21:48:07 -0500

    Update Module5.java

--- Module5.java
+++ Module5.java
@@ -7,7 +7,6 @@ public class Module5 {
         if (currentSpot == userInputs.length) {
             return 1;
         }
-
         return userInputs[currentSpot] *
                 multiplyNumbers(userInputs, currentSpot + 1);
     }
@@ -15,13 +14,10 @@ public class Module5 {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-
         int[] userInputs = new int[5];
-
         System.out.println("Please enter 5 numbers:");
 
         for (int i = 0; i < userInputs.length; i++) {
-
             while (true) {
                 System.out.print("Number " + (i + 1) + ": ");
 
@@ -36,9 +32,7 @@ public class Module5 {
         }
 
         int answer = multiplyNumbers(userInputs, 0);
-
         System.out.println("\nThe product is: " + answer);
-
         input.close();
     }
 }
